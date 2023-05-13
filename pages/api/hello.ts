b0dd33ea4ba8c5d1c424fecd6851915fe19b18bb
Christine-Only <tsy17321289030@163.com>
2023-05-13 20:36:22 +0800

    mock data

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { mock, Random } from 'mockjs';
 
 type Data = {
-  name: string
-}
+  name: string,
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(
+    mock({
+      'list|1-10': [
+        {
+          'id|+1': 0,
+          'name|1': [
+            Random.cname(),
+            Random.cname(),
+            Random.cname(),
+            Random.cname(),
+          ],
+          'birthday|1': [Random.date('yyyy-MM-dd'), Random.date('yyyy-MM-dd')],
+          address: Random.county(true),
+        },
+      ],
+    })
+  );
 }
