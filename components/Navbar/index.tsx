70155fed213e8accc2568ef28da4471de357f333
Christine-Only <tsy17321289030@163.com>
2023-05-13 21:55:37 +0800

    navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,7 +1,27 @@
 import type { NextPage } from 'Next';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { navs } from './config';
+
+import styles from './index.module.scss';
 
 const Navbar: NextPage = () => {
-  return <div>navbar</div>;
+  const { pathname, push } = useRouter();
+
+  return (
+    <div className={styles.navbar}>
+      <section className={styles.logArea}>BLOG-C</section>
+      <section className={styles.linkArea}>
+        {navs?.map((nav) => (
+          <Link key={nav.label} href={nav.value}>
+            <a className={pathname === nav.value ? styles.active : ''}>
+              {nav.label}
+            </a>
+          </Link>
+        ))}
+      </section>
+    </div>
+  );
 };
 
 export default Navbar;
